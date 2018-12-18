26b1ecc37a5a4334331ce850c8d81926c0f366ea
mxvl <maxime.faubert@gmail.com>
2018-12-18 17:56:19 -0500

    Update setup salon

--- i3/secDisplay.sh
+++ i3/secDisplay.sh
@@ -3,6 +3,8 @@
 output=$(xrandr -q | grep "\bconnected\b" | wc -l)
 
 #if [ $screen == 2 ]; then
-	~/.screenlayout/salon.sh
+	xrandr --output HDMI1 --mode 800x600 --pos 0x0 --rotate normal --output DP1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x600 --rotate normal --output VIRTUAL1 --off
+
 	nitrogen --restore
+	polybarmxvl
 #fi
