8c3ead9864c89b0136e989c89ea62f967ab4be85
mxvl <maxime.faubert@gmail.com>
2023-05-18 13:08:17 -0400

    .

--- i3/secDisplay.sh
+++ i3/secDisplay.sh
@@ -3,8 +3,9 @@
 output=$(xrandr -q | grep "\bconnected\b" | wc -l)
 
 #if [ $screen == 2 ]; then
-	xrandr --output HDMI1 --mode 800x600 --pos 0x0 --rotate normal --output DP1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x600 --rotate normal --output VIRTUAL1 --off
+#xrandr --output HDMI1 --mode 800x600 --pos 0x0 --rotate normal --output DP1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x600 --rotate normal --output VIRTUAL1 --off
+portable_droite.sh
 
-	nitrogen --restore
-	polybarmxvl
+nitrogen --restore &
+polybarmxvl &
 #fi
